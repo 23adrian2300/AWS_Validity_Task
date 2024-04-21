0f71f35c746bdea79ab7a553a5cc9b4d5544eb1b
23adrian2300 <adrianzerebiec23@gmail.com>
2024-04-21 18:50:28 +0200

    Updated version of functions

--- main.py
+++ main.py
@@ -1,23 +1,49 @@
 from verify_aws_validity import is_json, is_aws_json, check_asteriks
 import sys
+
 if __name__ == '__main__':
     path = input("Enter the path to the file: ")
-    if is_json(path) == "NOT JSON":
-        print("This is not a json file")
+    if is_json(path) == "ERROR":
+        print("This is not a valid json file")
         sys.exit(1)
-    elif is_json(path):
-        print("This is a json file")
-    else:
+    elif is_json(path) == "FILE NOT EXIST":
         print("File not exist")
         sys.exit(1)
-    if is_aws_json(path):
-        print("This is a valid AWS json file")
-    else:
-        print("This is not a valid AWS json file")
+    res = is_aws_json(path)
+    if res == "INVALID EFFECT":
+        print("There is invalid Effect field")
         sys.exit(1)
-    if check_asteriks(path):
-        print("There is no single * in the Resource field")
-    else:
-        print("There is * in the Resource field")
+    elif res == "INVALID VERSION":
+        print("There is invalid Version field")
         sys.exit(1)
-    print("End of program")
+    elif res == "INVALID POLICY NAME":
+        print("There is invalid Policy Name field")
+        sys.exit(1)
+    elif res == "NO ACTION FIELD":
+        print("There is no Action field")
+        sys.exit(1)
+    elif res == "NO RESOURCE FIELD":
+        print("There is no Resource field")
+        sys.exit(1)
+    elif res == "NO VERSION OR STATEMENT FIELD":
+        print("There is no Version or Statement field")
+        sys.exit(1)
+    elif res == "INVALID JSON FORMAT":
+        print("This is not a valid json file")
+        sys.exit(1)
+    elif res == "FILE NOT EXIST":
+        print("File not exist")
+        sys.exit(1)
+
+    res = check_asteriks(path)
+    if res == "JSON ERROR":
+        print("This is not a valid json file")
+        sys.exit(1)
+    elif res == "FILE NOT EXIST":
+        print("File not exist")
+        sys.exit(1)
+    elif res == True:
+        print("There are not any single asterisk in the Resource field")
+    else:
+        print("There is a single asterisk in the Resource field")
+        sys.exit(1)
--- verify_aws_validity.py
+++ verify_aws_validity.py
@@ -1,37 +1,64 @@
 import json
+import re
 
 
 def is_json(file_path):
     try:
         with open(file_path, 'r') as f:
-            try:
-                json.load(f)
-            except json.JSONDecodeError as e:
-                return "NOT JSON"
-            return True
+            json.load(f)
+        return True
+
     except FileNotFoundError as e:
-        return False
+        return "FILE NOT EXIST"
+    except json.JSONDecodeError as e:
+        return "JSON ERROR"
 
 
 def is_aws_json(file_path):
-    if not is_json(file_path) or is_json(file_path) == "NOT JSON":
-        return False
+    check = is_json(file_path)
+    if check == "FILE NOT EXIST" or check == "JSON ERROR":
+        return check
+
     try:
         with open(file_path, 'r') as file:
             data = json.load(file)
-            if "PolicyDocument" in data:
-                if "Version" in data["PolicyDocument"] and "Statement" in data["PolicyDocument"]:
-                    for key in data["PolicyDocument"]["Statement"]:
-                        if "Resource" in key:
-                            return True
-            return False
+
+            policy_name = data["PolicyName"]
+            policy_document = data["PolicyDocument"]
+            role = data["RoleName"]
+            if 1 < len(policy_name) > 128 or not re.fullmatch(r"[\w+=,.@-]+", policy_name):
+                return "INVALID POLICY NAME"
+
+            version = policy_document["Version"]
+            statement = policy_document["Statement"]
+
+            if version is None or statement is None:
+                return "NO VERSION OR STATEMENT FIELD"
+
+            if version not in ["2012-10-17", "2008-10-17"]:
+                return "INVALID VERSION"
+
+            for key in statement:
+                if "Action" not in key:
+                    return "NO ACTION FIELD"
+                if "Effect" in key:
+                    if key["Effect"] not in ["Allow", "Deny"]:
+                        return "INVALID EFFECT"
+                if "Resource" not in key:
+                    return "NO RESOURCE FIELD"
+
+            return True
+
     except FileExistsError as e:
-        return "File not found"
+        return "FILE NOT EXIST"
+    except json.JSONDecodeError as e:
+        return "INVALID JSON FORMAT"
 
 
 def check_asteriks(file_path):
-    if not is_aws_json(file_path):
-        return False
+    res = is_aws_json(file_path)
+    if res != True:
+        return res
     try:
         with open(file_path, 'r') as file:
             data = json.load(file)
@@ -40,4 +67,4 @@ def check_asteriks(file_path):
                     return False
             return True
     except FileExistsError as e:
-        return False
+        return "FILE NOT EXIST"
